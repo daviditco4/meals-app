549722802dece4977e075109b325d54ddb21fabe
David Itcovici <daviditco4@gmail.com>
2020-10-25 18:52:14 -0300

    Add navigation to the category meals page

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'category_meals_page.dart';
+
 class CategoryItem extends StatelessWidget {
   const CategoryItem(this.title, this.color);
 
   final String title;
   final Color color;
 
+  void pushCategoryMealsPage(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => CategoryMealsPage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.7), color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    final borderRadius = BorderRadius.circular(15.0);
+
+    return InkWell(
+      onTap: () => pushCategoryMealsPage(context),
+      borderRadius: borderRadius,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: borderRadius,
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.7), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
+        padding: const EdgeInsets.all(15.0),
+        child: Text(title, style: Theme.of(context).textTheme.headline6),
       ),
-      padding: const EdgeInsets.all(15.0),
-      child: Text(title, style: Theme.of(context).textTheme.headline6),
     );
   }
 }
---
+++ lib/category_meals_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Category Name')),
+      body: const Center(child: const Text('The category meals!')),
+    );
+  }
+}
